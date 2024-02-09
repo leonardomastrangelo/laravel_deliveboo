7945ceeeacf8e6455a88f30745e37d53cb8ac074
giuliamari04 <giuliamari04@gmail.com>
2024-02-09 10:41:16 +0100

    crud user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = config('db.user');
+        foreach ($users as $user) {
+            $newUser = new User();
+            $newUser->name = $user['name'];
+            $newUser->surname = $user['surname'];
+            $newUser->email =$user['email'];
+            $newUser->password = $user['password'];
+            $newUser->is_owner = $user['is_owner'];
+            $newUser->save();
+        }
+    }
+}
