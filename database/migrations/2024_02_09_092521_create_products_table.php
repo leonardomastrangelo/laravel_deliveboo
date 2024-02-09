9860e0d698a1e15835838544f75b329aa34ab155
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2024-02-09 10:46:25 +0100

    product table

---
+++ database/migrations/2024_02_09_092521_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 200);
+            $table->decimal(6, 2);
+            $table->string('image', 255)->nullable();
+            $table->string('ingredients', 255);
+            $table->boolean('availability');
+            $table->unsignedBigInteger('restaurant_id');
+            $table->foreign('restaurant_id')->references('id')->on('restaurant')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
