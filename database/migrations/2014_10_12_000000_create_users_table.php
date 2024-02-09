b7d4ec0c8929d927ac3bcf9e8c78bc69cfc5f317
giuliamari04 <giuliamari04@gmail.com>
2024-02-09 11:01:25 +0100

    correzione crud user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name',255);
-            $table->string('surname',255);
+            $table->string('surname', 255);
             $table->string('email',255)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password',255);
