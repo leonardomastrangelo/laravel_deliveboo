7b85ad777da9b647d8602df752a9062877299955
Eecodev <mnasser.bbk@gmail.com>
2024-02-10 12:23:02 +0100

    Crud products added

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Product;
+use App\Models\Restaurant;
 
 class ProductController extends Controller
 {
@@ -14,7 +16,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all();
+        return view ('admin.products.index', compact('products'));
     }
 
     /**
@@ -22,7 +25,8 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $restaurants = Restaurant::all();
+        return view('admin.products.create', compact('restaurants'));
     }
 
     /**
@@ -30,7 +34,17 @@ class ProductController extends Controller
      */
     public function store(StoreProductRequest $request)
     {
-        //
+        $formData = $request->validated();
+
+        if ($request->hasFile('image')) {
+            $path = Storage::put('images', $formData['image']);
+            $formData['image'] = $path;
+        }
+        $product = Product::create($formData);
+        if ($request->has('restaurants')){
+            $product->restaurants()->attach($request->restaurants);
+        }
+        return redirect()->route('admin.products.show', $product->id);
     }
 
     /**
@@ -38,7 +52,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return view ('admin.products.show', compact('product'));
     }
 
     /**
@@ -46,7 +60,8 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        $restaurants = Restaurant::all();
+        return view('admin.products.edit', compact('product', 'restaurant'));
     }
 
     /**
@@ -54,7 +69,17 @@ class ProductController extends Controller
      */
     public function update(UpdateProductRequest $request, Product $product)
     {
-        //
+        $formData = $request->validated();
+
+        if ($request->hasFile('image')) {
+            $path = Storage::put('images', $formData['image']);
+            $formData['image'] = $path;
+        }
+        $product->update($formData);
+        if ($request->has('restaurants')){
+            $product->restaurants()->sync($request->restaurants);
+        }
+        return redirect()->route('admin.products.show', $product->id);
     }
 
     /**
@@ -62,6 +87,10 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        if ($product->image){
+            Storage::delete($product->image);
+        }
+        $product->delete();
+        return to_route('admin.products.index')->with('message', "$product->title eliminato con successo");
     }
 }
