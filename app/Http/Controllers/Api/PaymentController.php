e6c02f8e95616f74eb598f99edf6f21ade34543b
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2024-02-17 15:42:29 +0100

    braintree

---
+++ app/Http/Controllers/Api/PaymentController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Braintree\Gateway;
+
+class PaymentController extends Controller
+{
+    public function process(Request $request)
+    {
+        $gateway = new Gateway([
+            'environment' => 'sandbox',
+            'merchantId' => '5pbryjzfb6jtngmk',
+            'publicKey' => 'wp5dnr224gm4wb4t',
+            'privateKey' => '0cf2f24a03ec81ad4b745f9ab485f120'
+        ]);
+
+        $nonce = $request->input('paymentMethodNonce');
+        $amount = '10.00'; // Specifica l'importo del pagamento
+        $result = $gateway->transaction()->sale([
+            'amount' => $amount,
+            'paymentMethodNonce' => $nonce,
+            'options' => ['submitForSettlement' => true]
+        ]);
+
+        if ($result->success) {
+            return response()->json(['success' => true]);
+        } else {
+            return response()->json(['success' => false, 'message' => $result->message]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\RestaurantController;
+use App\Http\Controllers\Api\PaymentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/restaurants', [RestaurantController::class, 'index']);
 Route::get('/restaurants/{slug}', [RestaurantController::class, 'show']);
+
+Route::post('/process-payment', [PaymentController::class, 'process']);
+
