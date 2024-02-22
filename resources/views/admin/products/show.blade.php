1bbfb89e4e80decbfa68009a54f30f00dcaa7a5c
GiorgioBolzoni <gbolzoni96@gmail.com>
2024-02-22 16:15:15 +0100

    sistemata grafica crud restaurants dashboard e products

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -1,58 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-    <section id="products-show" class="container-fluid">
-
-        <div class="row d-flex flex-wrap px-3 mt-5">
-            {{-- image --}}
-            <div class="col-12 col-md-3">
-                <img src="{{ asset('storage/' . $product->image) }}" class="w-100 d-block" alt="{{ $product->name }}">
-            </div>
-            <div class="col">
-                {{-- name --}}
-                <h2>
-                    {{ $product->name }}
-                </h2>
-                {{-- ingredients --}}
-                <p>
-                    {{ $product->ingredients }}
-                </p>
-                <ul>
-                    {{-- price --}}
-                    <li>
-                        {{ $product->price }} €
-                    </li>
-                    {{-- availability --}}
-                    <li>
-                        @if ($product->availability)
-                            Disponibile
-                        @else
-                            Non disponibile
-                        @endif
-                    </li>
-                </ul>
-                {{-- operations --}}
-                @if (auth()->user()->id === $restaurant->user_id)
-                    <div class="d-flex justify-content-center align-items-center">
-                        <a href="{{ route('admin.products.edit', $product->id) }}" class="btn btn-warning mx-3">
-                            <i class="fa-solid fa-pen-to-square"></i>
-                        </a>
-
-                        <form action="{{ route('admin.products.destroy', $product->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-
-                            <a id='button_deletep' class="btn btn-danger" type="submit"
-                                data-item-title='{{ $product->name }}' data-bs-toggle="modal"
-                                data-bs-target="#deleteModal">
-                                <i class="fa-solid fa-trash"></i></a>
-                        </form>
+    <section id="products-show" class="container-fluid py-5">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-5">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-12">
+                            <img src="{{ asset('storage/' . $product->image) }}" class="w-100 d-block"
+                                alt="{{ $product->name }}">
+                        </div>
+                        <div class="card-body row">
+                            <h2 class="card-title  col-md-12">{{ $product->name }}</h2>
+                            <p class="card-text  col-md-12">Ingredienti: <span> {{ $product->ingredients }} </span></p>
+                            <div class="col-md-12">
+                                <div>Prezzo: {{ $product->price }} €</div>
+                                <div>
+                                    @if ($product->availability)
+                                        Disponibile
+                                    @else
+                                        Non disponibile
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                @endif
+                    @if (auth()->user()->id === $restaurant->user_id)
+                        <div class="card-footer d-flex justify-content-center align-items-center flex-wrap">
+                            <a href="{{ route('admin.products.edit', $product->id) }}" class="btn btn-warning mx-3 my-3">
+                                <i class="fa-solid fa-pen-to-square"></i> Modifica
+                            </a>
+                            <form action="{{ route('admin.products.destroy', $product->id) }}" method="POST"
+                                class="my-3">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger" type="submit">
+                                    <i class="fa-solid fa-trash"></i> Elimina
+                                </button>
+                            </form>
+                        </div>
+                    @endif
+                </div>
             </div>
-
         </div>
         @include('partials.modal_delete')
     </section>
 @endsection
-
