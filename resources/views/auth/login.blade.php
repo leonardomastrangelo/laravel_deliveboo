81427b2875bca764e67f09994840e6c05f1026ed
giovannimegliola <giovanni.megliola@hotmail.it>
2024-02-23 10:30:04 +0100

    modified img path

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -77,7 +77,7 @@
                                 </form>
                             </div>
                             <div class=" col-6 image-login">
-                                <img src="{{ asset('/resources/img/deliverooLogin.gif') }}" class="w-100 d-block"
+                                <img src="{{ Vite::asset('/resources/img/deliverooLogin.gif') }}" class="w-100 d-block"
                                     alt="image">
                             </div>
                         </div>
